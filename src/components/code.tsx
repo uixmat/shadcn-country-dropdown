835329ce6216350c72707757544213669bd55d7f
uixmat <mattsince87@gmail.com>
2024-11-09 19:58:48 +0200

    scroll code blocks

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -4,7 +4,7 @@ export async function Code({ code, lang }: { code: string; lang?: string }) {
   const highlightedCode = await highlightCode(code, lang);
   return (
     <section
-      className="max-h-96 overflow-y-scroll rounded-sm overflow-clip [&>pre]:p-4 [&>pre]:text-sm [&>pre]:rounded-none [&>pre]:leading-6 [&>pre]:bg-muted"
+      className="max-h-96 overflow-clip overflow-y-scroll rounded-sm [&>pre]:p-4 [&>pre]:text-sm [&>pre]:rounded-none [&>pre]:leading-6 [&>pre]:bg-muted [&>pre]:overflow-scroll"
       dangerouslySetInnerHTML={{
         __html: highlightedCode,
       }}
